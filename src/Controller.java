f862525572b69e56388498e8b3dc8eba6d71c1c4
ToytonelloEdu / AntoCiroRepo <antonio.ascione15@studenti.unina.it>
2023-01-04 18:18:11 +0100

    Removed testing

--- src/Controller.java
+++ src/Controller.java
@@ -1,22 +1,8 @@
-import Concrete_classes.*;
-import DAO_classes.*;
-import com.sun.tools.javac.Main;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.*;
-
 public class Controller {
     CF_MainFrame MainFrame;
 
     public static void main(String[] args) {
         //Controller business_logic = new Controller();
-        Sede temp = new Sede("Casa Ascione", "Via Cupa Camaldoli, 18", "Torre del Greco");
-        Sede Sede_new = new Sede("Casa Anastasio", "Via San Tommaso Apostolo, 9", "Pomigliano D'Arco");
-        Sede_DAO dao_temp = new Sede_DAO();
-        dao_temp.UpdateSede(temp, Sede_new);
     }
 
     public Controller(){
