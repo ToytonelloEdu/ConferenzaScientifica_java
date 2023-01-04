83a50791a8c31b765a0570d7751060bea4b631fd
CiroAnastasio <cir.anastasio@studenti.unina.it>
2023-01-04 19:06:40 +0100

    Inizio Locazione e Insert

--- src/DAO_classes/Locazione_DAO.java
+++ src/DAO_classes/Locazione_DAO.java
@@ -1,4 +1,46 @@
 package DAO_classes;
+import java.sql.*;
+import java.util.*;
+import Model_classes.Locazione;
+import Model_classes.Sede;
 
 public class Locazione_DAO {
+
+    public Locazione_DAO(){
+
+    }
+    public Statement getStatement() throws SQLException {
+        try{
+            DBConnection dbConnection = DBConnection.getDBConnection();
+
+            Connection conn = dbConnection.getConnection();
+            if (conn == null)
+                throw new SQLException();
+
+            // definisce lo statement
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate("SET search_path TO Main");
+            return stmt;
+        }
+        catch(SQLException throwables){
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+
+    public void InsertLocazione(Locazione Locazione_temp){
+        try {
+            Sede Sede_temp = Locazione_temp.getCollocazione();
+            Sede_DAO Sede_DAO_temp = Sede_temp.getDao();
+            Integer Pk_Sede_Locazione = Sede_DAO_temp.getPK(Sede_temp);
+            Statement LocalStatement = this.getStatement();
+            String command = "INSERT INTO Main.Locazione VALUES ('"+ Pk_Sede_Locazione.toString() + "', " +Locazione_temp.toSQLrow() +");";
+
+            LocalStatement.execute(command);
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
--- src/Model_classes/Locazione.java
+++ src/Model_classes/Locazione.java
@@ -1,4 +1,45 @@
 package Model_classes;
 
+import DAO_classes.Sede_DAO;
+
 public class Locazione {
+    private Sede Collocazione;
+    private String Nome;
+    private int PostiDisponibili;
+
+    public Locazione(Sede Collocazione, String Nome, int PostiDisponibili){
+        this.Collocazione = Collocazione;
+        this.Nome = Nome;
+        this.PostiDisponibili = PostiDisponibili;
+    }
+
+    public Sede getCollocazione() {
+        return Collocazione;
+    }
+
+    public void setCollocazione(Sede collocazione) {
+        Collocazione = collocazione;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public int getPostiDisponibili() {
+        return PostiDisponibili;
+    }
+
+    public void setPostiDisponibili(int postiDisponibili) {
+        PostiDisponibili = postiDisponibili;
+    }
+
+    public String toSQLrow() {
+        String ret = "'"+ this.Nome +"', '"+ this.PostiDisponibili + "'";
+        return ret;
+    }
+
 }
