509c561d7aa9575075b5fdee9bc98869c827d746
ToytonelloEdu / AntoCiroRepo <antonio.ascione15@studenti.unina.it>
2023-01-04 11:54:24 +0100

    First version

---
+++ src/Controller.java
@@ -0,0 +1,12 @@
+import com.sun.tools.javac.Main;
+
+public class Controller {
+    CF_MainFrame MainFrame;
+
+    public static void main(String[] args) {
+        CF_MainFrame MainFrame  = new CF_MainFrame();
+    }
+
+    public Controller(){
+    }
+}
